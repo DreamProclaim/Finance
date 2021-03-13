db1aa4cfb321f73fd992493c50c0b84f11e08987
DreamPing <15730536920@163.com>
2021-03-13 23:49:35 +0800

    *修改

--- Finance.Core/IOCConfiger.cs
+++ Finance.Core/IOCConfiger.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 
 namespace Finance.Core
 {
