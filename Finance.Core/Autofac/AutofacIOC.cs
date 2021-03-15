f66f7a7fdc0ad00255d0a13e27311b397987fef6
Dream <15730536920@139.com>
2021-03-15 16:03:43 +0800

    *添加Autofac

---
+++ Finance.Core/Autofac/AutofacIOC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Finance.Core.Autofac
+{
+    public class AutofacIOC
+    {
+        private static IContainer Container;
+        public static void InitAutofac()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("Json/Configer.json");
+            var module = new ConfigurationModule(config.Build());
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(module);
+            Container = builder.Build();
+        }
+        // <summary>
+        /// 从容器中获取对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public static T GetFromFac<T>()
+        {
+            return Container.Resolve<T>();
+            //   return (T)DependencyResolver.Current.GetService(typeof(T));
+        }
+
+        public static T GetFromFac<T>(string name)
+        {
+            return Container.ResolveNamed<T>(name);
+        }
+    }
+}
