9b618f185811b085fb2cad02519e66a1acd2413f
Dream <15730536920@139.com>
2021-03-17 10:06:30 +0800

    *删除关闭窗体

--- Finance.UI/User/UserAdd.cs
+++ Finance.UI/User/UserAdd.cs
@@ -21,10 +21,16 @@ namespace Finance.UI.User
 
         private void UserAdd_Load(object sender, EventArgs e)
         {
+            this.skinLabel1.Text = "手机号：";
+            this.skinLabel2.Text = "姓名：";
+            this.skinLabel3.Text = "密码：";
+            this.skinLabel4.Text = "性别：";
+            this.skinLabel5.Text = "图片：";
         }
 
         private void UserAdd_FormClosing(object sender, FormClosingEventArgs e)
         {
+            this.Show();
             WindowsClose windows = new WindowsClose();
             windows.Show();
         }
